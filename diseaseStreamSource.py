15618be0a846edebe30412acf85c3096228aef33
mayankmetha <mayankmethad@gmail.com>
2020-02-21 09:07:38 +0530

    Added a stream source and processing engine

---
+++ diseaseStreamSource.py
@@ -0,0 +1,21 @@
+import socket
+import time
+from random import uniform
+
+host = 'localhost'
+port = 9998
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(1)
+
+try:
+    conn, addr = s.accept()
+    try:
+        while True:
+            conn.send(bytes("{0:.0f},{1:.0f}\n".format(uniform(-180,180),uniform(-90,90)), "utf-8"))
+            time.sleep(uniform(0,2))
+    except:
+        conn.close()
+except KeyboardInterrupt:
+    s.close()
