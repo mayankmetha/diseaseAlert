8c35b14fbbb487f1e911a15ed5e22e96559ef409
mayankmetha <mayankmethad@gmail.com>
2020-02-23 23:05:57 +0530

    Combining streams

--- diseaseStreamSource.py
+++ diseaseStreamSource.py
@@ -13,7 +13,7 @@ try:
     conn, addr = s.accept()
     try:
         while True:
-            conn.send(bytes("{0:.0f},{1:.0f}\n".format(uniform(-180,180),uniform(-90,90)), "utf-8"))
+            conn.send(bytes("{0:.2f},{1:.2f}\n".format(uniform(-180,180),uniform(-90,90)), "utf-8"))
             time.sleep(uniform(0,2))
     except:
         conn.close()
